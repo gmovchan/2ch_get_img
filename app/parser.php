5d6da5d15fd8f4eb1af2dd30b241f3c4932e515d
grigory <grigory08@ya.ru>
2017-09-10 18:48:14 +0300

    добавил тодо

--- app/parser.php
+++ app/parser.php
@@ -2,105 +2,43 @@
 
 namespace Application;
 
-class Parser2ch
+class ThreadParser
 {
-
-    private $host2ch = "2ch.hk";
 
     function __construct()
     {
         
     }
 
-    /*
-     * Проверяет является ли строка ссылкой на тред
-     * Подозреваю что с помощью регулярного выражения было бы гораздо короче
-     */
-
-    public function validateLink($link)
+    public function parseThread(ThreadDownloader $downloader, $url)
     {
-        if (empty($link)) {
-            return false;
-        }
-
-        $urlArray = parse_url($link);
-
-        if (!isset($urlArray["host"])) {
-            return false;
-        }
-
-        if ($urlArray["host"] !== $this->host2ch) {
-            return false;
-        }
-
-        if (!isset($urlArray["path"])) {
-            return false;
-        }
-
-        $pathArray = explode('/', $urlArray["path"]);
-
-        if (!isset($pathArray[3]) || empty($pathArray[3])) {
-            return false;
-        }
-
-        if ($pathArray[2] !== "res") {
-            return false;
-        }
-
-        $threadLink = explode('.', $pathArray[3]);
-
-        if (count($threadLink) != 2) {
-            return false;
-        }
-
-        if (!is_numeric($threadLink[0]) || $threadLink[1] !== "html") {
-            return false;
-        }
-
-        return true;
-    }
-
-    public function parseThread($link)
-    {
-        if ($this->validateLink($link) === FALSE) {
-            echo "Не является ссылкой на тред: \"$link\".";
-        } else {
-            echo 'Ссылка правильная.';
-            $code = $this->getHtmlCodeUrl($link);
-            
-            if (is_null($code)) {
-                return false;
-            }
-            
-            echo '<hr>';
-            echo htmlspecialchars($code);
-        }
-    }
-
-    /*
-     * Скачивает html-код треда для последующего разбора
-     */
-
-    private function getHtmlCodeUrl($url)
-    {
-        $curl = curl_init(); // Инициализирую CURL
-        curl_setopt($curl, CURLOPT_HEADER, 0); // Отключаю в выводе header-ы
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //Возвратить данные а не показать в браузере
-        curl_setopt($curl, CURLOPT_URL, $url); // Указываю URL
-        $code = curl_exec($curl); // Получаю данные
-        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        curl_close($curl); // Закрываю CURL сессию
-
-        if ($httpCode !== 200) {
-            echo "<br>";
-            echo "Файл не найден.";
+        $code = $downloader->getThreadHtmlCode($url);
+        if (is_null($code)) {
             return null;
         }
 
-        echo "<br>";
-        echo "Файл найден.";
-        return $code;
+        $dom = new \DOMDocument();
+        libxml_use_internal_errors(true); // скрывает сообщения об ошибках HTML
+        $dom->loadHTML($code);
+                
+        $nodes = $dom->getElementsByTagName("div");
+        $sourceImageLinks = array();
+
+        foreach ($nodes as $element) {
+            $classy = $element->getAttribute("class");
+
+            if (strpos($classy, "image") !== false) {
+                $tagsA = $element->getElementsByTagName("a");
+
+                foreach ($tagsA as $tagA) {
+                    $sourceImageLinks[] = $tagA->getAttribute("href");
+                }
+            }
+        }
+        
+        $downloader->downloadImages($sourceImageLinks);
+        
+        return "Файлы скачаны.";
     }
 
 }
