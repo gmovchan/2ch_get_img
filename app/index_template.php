40bd16868e52d3d3b878cebd9493cbe1b91f8ba3
grigory <grigory08@ya.ru>
2017-09-10 03:56:24 +0300

    Новый проект

--- app/index_template.php
+++ app/index_template.php
@@ -15,10 +15,10 @@
             <div class="row h-100">
                 <div class="col-12 align-self-center">
                     <div class="w-50 mx-auto">
-                        <form method="post" action="index.php?action=parse">
+                        <form method="POST" action="index.php?action=parse">
                             <div class="form-group">
                                 <label for="input2chLink">Ссылка на тред</label>
-                                <input type="text" class="form-control" id="input2chLink" aria-describedby="input2chLink" placeholder="Enter 2ch thred link">
+                                <input type="text" class="form-control" id="input2chLink" name="input2chLink" aria-describedby="input2chLink" placeholder="Enter 2ch thred link">
                             </div>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
--- public/index.php
+++ public/index.php
@@ -7,12 +7,21 @@ require __DIR__ . '/../vendor/autoload.php';
 if (isset($_GET["action"])) {
     switch ($_GET["action"]) {
         case 'parse':
-            echo 'Start parse';
+
+            if (isset($_POST["input2chLink"])) {
+                echo 'Start parse.';
+                $parser = new Parser2ch();
+                echo '<br>';
+                $parser->parseThread($_POST["input2chLink"]);                
+            } else {
+                echo 'Ссылка не получена.';
+            }
+
             exit();
             break;
 
         default:
-            
+
             break;
     }
 }
